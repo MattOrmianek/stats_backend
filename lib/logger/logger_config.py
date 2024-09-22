a152a9c709d545c66cb13535e6c33e8d7a70a871
Mateusz Ormianek <mattormianek@gmail.com>
2024-09-22 04:22:16 +0200

    style: formatted logger_config

--- lib/logger/logger_config.py
+++ lib/logger/logger_config.py
@@ -2,9 +2,10 @@
 This module provides a logger configuration for the application.
 """
 
+from logging.handlers import RotatingFileHandler
 import logging
 import colorlog
-from logging.handlers import RotatingFileHandler
+
 
 def setup_logger(name):
     """
@@ -18,22 +19,21 @@ def setup_logger(name):
     console_handler.setLevel(logging.DEBUG)
 
     # File Handler
-    file_handler = RotatingFileHandler(
-        "backend.log", maxBytes=50 * 1024 * 1024, backupCount=50
-    )
+    file_handler = RotatingFileHandler("backend.log", maxBytes=50 * 1024 * 1024, backupCount=50)
     file_handler.setLevel(logging.DEBUG)
 
     # Formatter
     formatter = colorlog.ColoredFormatter(
-        "%(log_color)s%(asctime)s - %(name)s - %(filename)s:%(lineno)d - %(funcName)s - %(levelname)s - %(message)s",
+        "%(log_color)s%(asctime)s - %(name)s - %(filename)s:%(lineno)d - "
+        "%(funcName)s - %(levelname)s - %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
         log_colors={
-            'DEBUG': 'cyan',
-            'INFO': 'green',
-            'WARNING': 'yellow',
-            'ERROR': 'red',
-            'CRITICAL': 'bold_red',
-        }
+            "DEBUG": "cyan",
+            "INFO": "green",
+            "WARNING": "yellow",
+            "ERROR": "red",
+            "CRITICAL": "bold_red",
+        },
     )
 
     console_handler.setFormatter(formatter)
